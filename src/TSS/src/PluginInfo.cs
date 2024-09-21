7fcaf6a2d10aec549a0b203e6e1c01f3237aa236
O-Deka-K <56600803+O-Deka-K@users.noreply.github.com>
2024-09-20 22:01:31 -0500

    Changed to ArmSprint

--- src/TSS/src/PluginInfo.cs
+++ src/TSS/src/PluginInfo.cs
@@ -1,9 +1,31 @@
 namespace H3VRMod
 {
-	internal static class PluginInfo
-	{
-		internal const string NAME = "Twin Stick Swinger";
-		internal const string GUID = "dll.h3vr.potatoes.twinstickswinger";
-		internal const string VERSION = "1.1.0";
-	}
+    // Based on Potatoes' Twin Stick Swinger
+    // Forked from https://github.com/potatoes1286/TwinStickSwinger
+    // This version replaces Armswinger mode instead of Dash
+
+    // It respects the following settings:
+    // - TwinStick Options:
+    //   - Movement Speed
+    //   - Controller Forward/Side Root
+    //   - TwinStick Left/Right Handedness
+    //   - TwinStick Jump
+    // - ArmSwinger Options:
+    //   - ArmSwinger Jump
+    //   - ArmSwinger Turning Mode
+
+    // It IGNORES the following settings:
+    // - TwinStick Options:
+    //   - TwinStick Turning Mode
+    //   - TwinStick Sprint Mode
+    //   - TwinStick Sprint Toggle Mode
+    // - ArmSwinger Options:
+    //   - ArmSwinger Base Speed (Left Hand)
+    //   - ArmSwinger Base Speed (Right Hand)
+    internal static class PluginInfo
+    {
+        internal const string NAME = "TwinStick Arm Sprint";
+        internal const string GUID = "dll.h3vr.odekak.twinstickarmsprint";
+        internal const string VERSION = "1.5.0";
+    }
 }
